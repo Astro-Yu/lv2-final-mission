1cb4173f1fed11bac411e37121f4c5413e007789
yush <yush2202@gmail.com>
2025-06-17 17:38:53 +0900

    feat: 랜덤 이름 외부 api 구현

--- src/main/java/finalmission/config/RandommerProperties.java
+++ src/main/java/finalmission/config/RandommerProperties.java
@@ -3,5 +3,5 @@ package finalmission.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "randommer")
-public record RandommerProperties(String baseUrl, String apiKey) {
+public record RandommerProperties(String baseUrl, String apiKey, int connectTimeout, int readTimeout) {
 }
